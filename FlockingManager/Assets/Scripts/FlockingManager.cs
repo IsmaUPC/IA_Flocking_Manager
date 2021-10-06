6a5a9a1a266c5575fcfe02b798faf96cdb196271
Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
2021-10-06 12:18:57 +0200

    Movement Basic

--- FlockingManager/Assets/Scripts/FlockingManager.cs
+++ FlockingManager/Assets/Scripts/FlockingManager.cs
@@ -7,22 +7,27 @@ public class FlockingManager : MonoBehaviour
     public GameObject fishPrefab;
     public int numFish = 20;
     public GameObject[] allFish;
-    public Vector3 swimLimits = new Vector3(5, 5, 5); 
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
 
-   
-       void Start () {
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
 
-           allFish = new GameObject[numFish];
-		for(int i = 0; i<numFish; i++)
 
+    void Start () {
+
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
         {
-        Vector3 pos = this.transform.position +
+            Vector3 pos = this.transform.position +
                 new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
                 Random.Range(-swimLimits.y, swimLimits.y),
                 Random.Range(-swimLimits.z, swimLimits.z));
-        allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
-        //allFish[i].GetComponent<Flock>().myManager = this;
-    }
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
 
     }
 
