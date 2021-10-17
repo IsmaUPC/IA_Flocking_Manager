2824204028f86c5ea70c415f6ef1666d8e226c87
IsmaUPC <itejada.UPC@gmail.com>
2021-10-17 16:22:53 +0200

    ia update

--- FlockingManager/Assets/Scripts/Flock.cs
+++ FlockingManager/Assets/Scripts/Flock.cs
@@ -4,93 +4,102 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-	public FlockingManager myManager;
-	float speed;
-	Vector3 direction = Vector3.zero;
+    Vector3 cohesion = Vector3.zero;
+    Vector3 align = Vector3.zero;
+    Vector3 vecLeader = Vector3.zero;
+    Vector3 separation = Vector3.zero;
 
-	public bool leader = false;
-	public Quaternion directionLeader = Quaternion.identity;
+    public FlockingManager myManager;
+    float speed;
+    Vector3 direction = Vector3.zero;
 
-	private float freq = 0f;
-	// Start is called before the first frame update
-	void Start()
-	{
-		speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
-	}
+    public bool leader = false;
 
-	// Update is called once per frame
-	void Update()
-	{
-		freq += Time.deltaTime;
-		if (freq > 0.5)
-		{
-			freq -= 0.5f;
-			ApplyRules();
-		}
-		// If this is a leader dictates a new random address
-		if (leader)
-		{
-			transform.rotation = directionLeader;
-		}
-		else if (direction != Vector3.zero)
-		{
-			transform.rotation = Quaternion.Slerp(transform.rotation,
-									  Quaternion.LookRotation(direction),
-									  myManager.rotationSpeed * Time.deltaTime);
-		}
+    private float freq = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
 
-		transform.Translate(0, 0, Time.deltaTime * speed);
-	}
-	void ApplyRules()
-	{
-		Vector3 cohesion = Vector3.zero;
-		Vector3 align = Vector3.zero;
-		Vector3 separation = Vector3.zero;
-		float distance;
-		int groupSize = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        freq += Time.deltaTime;
+        if (freq > 0.5)
+        {
+            freq -= 0.5f;
+            ApplyRules();
+        }
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                      Quaternion.LookRotation(direction),
+                                      myManager.rotationSpeed * Time.deltaTime);
+        }
 
-		float distancePredator = Vector3.Distance(myManager.predator.transform.position, transform.position);
-		if (distancePredator < 2)
-		{
-			direction = (transform.position - myManager.predator.transform.position).normalized * speed;
-			direction *= 5;
-		}
-		else
-		{
-			foreach (GameObject go in myManager.allFish)
-			{
-				//Check that the object is different from the current one  
-				if (go != this.gameObject)
-				{
-					//Calculate the distance to the neighbor 
-					distance = Vector3.Distance(go.transform.position, transform.position);
-					//If the distance is less than the marked limit
-					if (distance <= myManager.neighbourDistance)
-					{
-						// Calcualte cohesion (the total amount of the positions)
-						cohesion += go.transform.position;
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+    void ApplyRules()
+    {
+        if (!myManager.isLeader) FlockSwim();
+        else FollowLeader();
+    }
 
-						// Calculate align (the total amount of the directions)
-						// The leader has more influence over the other entities 
-						if (go.GetComponent<Flock>().leader)
-						{
-							align += go.GetComponent<Flock>().direction * this.myManager.numFish * 10;
-						}
-						else align += go.GetComponent<Flock>().direction;
+    private void FlockSwim()
+    {
+        cohesion = Vector3.zero;
+        align = Vector3.zero;
+        vecLeader = Vector3.zero;
+        separation = Vector3.zero;
 
-						// Avoid crowding neighbours
-						separation -= (transform.position - go.transform.position) / (distance * distance);
-						// Increase group size
-						groupSize++;
-					}
-				}
-			}
-			cohesion = (cohesion / groupSize - transform.position).normalized * speed;
-			align /= groupSize;
-			speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
-			// Combination for calculate new direction
-			direction = (cohesion + align + separation).normalized * speed;
-		}
-	}
+        float distance;
+        int groupSize = 0;
+
+        float distancePredator = Vector3.Distance(myManager.predator.transform.position, transform.position);
+        if (distancePredator < 3)
+        {
+            direction = (transform.position - myManager.predator.transform.position).normalized * speed;
+            direction *= 5;
+        }
+        else
+        {
+            foreach (GameObject go in myManager.allFish)
+            {
+                //Check that the object is different from the current one  
+                if (go != this.gameObject)
+                {
+                    //Calculate the distance to the neighbor 
+                    distance = Vector3.Distance(go.transform.position, transform.position);
+                    //If the distance is less than the marked limit
+                    if (distance <= myManager.neighbourDistance)
+                    {
+                        // Calcualte cohesion (the total amount of the positions)
+                        cohesion += go.transform.position;
+
+                        // Calculate align (the total amount of the directions)
+                        align += go.GetComponent<Flock>().direction;
+
+                        // Avoid crowding neighbours
+                        separation -= (transform.position - go.transform.position) / (distance * distance);
+                        // Increase group size
+                        groupSize++;
+                    }
+                }
+            }
+            cohesion = (cohesion / groupSize - transform.position).normalized * speed;
+            align /= groupSize;
+            speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
+            // Combination for calculate new direction
+            direction = (cohesion + align + separation + vecLeader).normalized * speed;
+            Debug.Log(direction);
+        }
+    }
+
+    // The leader has more influence over the other entities 
+    void FollowLeader()
+    {
+        direction = ((myManager.allFish[myManager.GetLeader()].transform.position) - transform.position).normalized * speed;
+    }
 
 }
